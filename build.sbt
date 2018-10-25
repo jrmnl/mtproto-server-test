1f836a7b51699a9932740675958daa86add62262
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-10-25 22:04:13 +0300

    auth process graph stage was realized

--- build.sbt
+++ build.sbt
@@ -6,3 +6,5 @@ scalaVersion := "2.12.7"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.17"
 libraryDependencies += "org.scodec" %% "scodec-core" % "1.10.3"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
---
+++ src/main/scala/ru/tolsi/mtproto/AuthProcess.scala
@@ -0,0 +1,55 @@
+package ru.tolsi.mtproto
+
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import com.typesafe.scalalogging.StrictLogging
+
+sealed trait AuthStep
+
+case object WaitForReqPq extends AuthStep
+
+case object WaitForReqDHParams extends AuthStep
+
+class AuthProcess extends GraphStage[FlowShape[MTProtoInMessage, MTProtoOutMessage]] with StrictLogging {
+
+  val in: Inlet[MTProtoInMessage] = Inlet[MTProtoInMessage]("in")
+  val out: Outlet[MTProtoOutMessage] = Outlet[MTProtoOutMessage]("out")
+
+  override val shape: FlowShape[MTProtoInMessage, MTProtoOutMessage] = FlowShape.of(in, out)
+
+  override def createLogic(attr: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+      private var step: AuthStep = WaitForReqPq
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          val msg = grab(in)
+          authLogic(step, msg) match {
+            case Right((nextStep, message)) =>
+              step = nextStep
+              push(out, message)
+            case Left(error) =>
+              logger.info(error)
+              completeStage()
+          }
+        }
+      })
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          pull(in)
+        }
+      })
+    }
+
+  private def authLogic(currentStep: AuthStep, message: MTProtoInMessage): Either[String, (AuthStep, MTProtoOutMessage)] = {
+    currentStep match {
+      case WaitForReqPq => message match {
+        case _: ReqPq => Right(WaitForReqDHParams -> ResPq.createRandom)
+        case _ => Left("I'm wait for ReqPq, not this")
+      }
+      case WaitForReqDHParams => {
+        case _: ReqPq => Left("I'm wait for ReqDHParams, not this")
+        case _: ReqDHParams => Left("Done!")
+      }
+    }
+  }
+}
